c455dfc928163ad772bd01b06daac319164cf195
Elaidzha1940 <eli080293@gmail.com>
2024-03-24 16:26:41 +0300

    VideoClipApp

---
+++ VideoClip/ContentView.swift
@@ -0,0 +1,22 @@
+//  /*
+//
+//  Project: VideoClip
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date 24.03.2024
+//
+//  */
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VideoClip/VideoClipApp.swift
@@ -0,0 +1,19 @@
+//  /*
+//
+//  Project: VideoClip
+//  File: VideoClipApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date 24.03.2024
+//
+//  */
+
+import SwiftUI
+
+@main
+struct VideoClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
