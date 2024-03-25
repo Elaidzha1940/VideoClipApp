36e84fe3df8c579778bc3b330cd7901efae95551
Elaidzha1940 <eli080293@gmail.com>
2024-03-25 20:41:59 +0300

    Added JSON file / videoData

--- VideoClip/Views/ContentView.swift
+++ VideoClip/Views/ContentView.swift
@@ -10,21 +10,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var videoManager = VideoManager()
+    
     var body: some View {
-        VStack {
-            HStack {
-                ForEach(Query.allCases, id: \.self) { searchQuery in
-                    QueryTag(query: searchQuery, isSelected: false)
-                    
+        NavigationStack {
+            VStack {
+                HStack {
+                    ForEach(Query.allCases, id: \.self) { searchQuery in
+                        QueryTag(query: searchQuery, isSelected: false)
+                        
+                    }
                 }
+                
+                ScrollView{
+                    ForEach(videoManager.videos, id: \.id) { video in
+                        NavigationLink {
+                            VideoCard(video: video)
+                        } label: {
+                            VideoCard(video: video)
+                        }
+                    }
+                }
+                .frame(maxWidth: .infinity)
             }
-            
-            ScrollView{
-                VideoCard(video: previewVideo)
-            }
-            .frame(maxWidth: .infinity)
+            .background(Color("AccentColor"))
         }
-        .background(Color("AccentColor"))
     }
 }
 
