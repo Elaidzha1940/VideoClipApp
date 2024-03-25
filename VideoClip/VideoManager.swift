5512e95465708f6b9cbf001403572860485e932d
Elaidzha1940 <eli080293@gmail.com>
2024-03-25 12:51:51 +0300

    Update VideoCard

--- VideoClip/VideoManager.swift
+++ VideoClip/VideoManager.swift
@@ -12,3 +12,12 @@ import Foundation
 enum Query: String, CaseIterable {
     case people, nature, animals, food, ocean
 }
+
+struct ResponseBody: Decodable {
+    var page: Int
+    var perPage: Int
+    var totalRes
+    var 
+    var 
+    
+}
