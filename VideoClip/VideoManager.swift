1188d8d8dfe1ab591dc0b8110ce72756e1b4aa2b
Elaidzha1940 <eli080293@gmail.com>
2024-03-25 16:49:37 +0300

    Update VideoManager

--- VideoClip/VideoManager.swift
+++ VideoClip/VideoManager.swift
@@ -16,8 +16,28 @@ enum Query: String, CaseIterable {
 struct ResponseBody: Decodable {
     var page: Int
     var perPage: Int
-    var totalRes
-    var 
-    var 
+    var totalResults: Int
+    var url: String
+    var videos: [Video]
+}
+
+struct Video: Identifiable, Decodable {
+    var id: Int
+    var image: String
+    var duration: Int
+    var user: User
+    var videoFile: [VideoFile]
+   
+    struct User: Identifiable, Decodable {
+        var id: Int
+        var name: String
+        var url: String
+    }
     
+    struct VideoFile: Identifiable, Decodable {
+        var id: Int
+        var quality: String
+        var fileType: String
+        var link: String
+    }
 }
