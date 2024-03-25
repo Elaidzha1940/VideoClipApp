0c4c070e2c4b9e84232ac934255bcb6eeee3c1c4
Elaidzha1940 <eli080293@gmail.com>
2024-03-25 19:06:31 +0300

    Added VideoModel

---
+++ VideoClip/Models/VideoModel.swift
@@ -0,0 +1,35 @@
+//  /*
+//
+//  Project: VideoClip
+//  File: VideoModel.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 25.03.2024
+//
+//  */
+ 
+import SwiftUI
+
+var previewVideo: Video = load("videoDate.json")
+
+func load<T: Decodable>(_ filename: String) -> T {
+    let data: Data
+
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+        else {
+            fatalError("Couldn't find \(filename) in main bundle.")
+    }
+
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
+    }
+
+    do {
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+    }
+}
